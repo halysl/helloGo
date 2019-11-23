c5a3cc5d6c1357c49f7b4d1a85e5b151ae1a3fb7
Yagami Light <halysl0817@gmail.com>
2019-11-23 18:11:52 +0800

    使用sync.mutex实现

---
+++ basis/use_mutex.go
@@ -0,0 +1,40 @@
+package basis
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeCounter struct {
+	v map[string]int
+	mux sync.Mutex
+}
+
+func (s *SafeCounter) Inc(key string) {
+	s.mux.Lock()
+	s.v[key]++
+	s.mux.Unlock()
+}
+
+func (s *SafeCounter) Reduce(key string) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v[key]--
+}
+
+func (s *SafeCounter) Value(key string) int {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	return s.v[key]
+}
+
+func Run_mutex() {
+	s := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go s.Inc("somekey")
+		go s.Reduce("somekey")
+	}
+	time.Sleep(time.Second)
+	fmt.Println(s.Value("somekey"))
+}
