101f39c05a91bfa74bedaffb19871498f177c540
light <light@LightdeMacBook-Pro.local>
2019-11-28 17:19:05 +0800

    gin first

---
+++ gin_study/run.go
@@ -0,0 +1,16 @@
+package gin_study
+
+import "github.com/gin-gonic/gin"
+
+func Run() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run() // listen and serve on 0.0.0.0:8080
+	if err != nil {
+		panic(err)
+	}
+}
