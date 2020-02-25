fdb60c786d29c0aeb40460a69bc71684f4d37ff9
light <light@LightdeMacBook-Pro-2.local>
2020-02-25 15:47:51 +0800

    add set-get

--- basis/proctice/studentmanager.go
+++ basis/proctice/studentmanager.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type student struct {
 	ID         int
@@ -43,11 +46,17 @@ func (c *class) listStudent() {
 	}
 }
 
-// func (s *student) editInfo(key string, value interface{}) {
-// 	rs := reflect.ValueOf(s).Elem()
-// 	fmt.Println(rs)
-// 	rs.FieldByName(key).Set(reflect.ValueOf(value))
-// }
+func (s *student) setName(val string) {
+	reflect.ValueOf(s).Elem().FieldByName("Name").SetString(val)
+}
+
+func (s *student) setAge(val int) {
+	reflect.ValueOf(s).Elem().FieldByName("Age").SetInt(int64(val))
+}
+
+func (s *student) setScore(val int) {
+	reflect.ValueOf(s).Elem().FieldByName("Score").SetInt(int64(val))
+}
 
 func studentManagerTest() {
 	c := class{students: make([]*student, 0, 200)}
@@ -70,4 +79,13 @@ func studentManagerTest() {
 	fmt.Println("移除一个学生")
 	c.removeStudent(20)
 	c.listStudent()
+
+	c.students[0].setName("test")
+	fmt.Println(c.students[0])
+
+	c.students[0].setAge(20)
+	fmt.Println(c.students[0])
+
+	c.students[0].setScore(150)
+	fmt.Println(c.students[0])
 }
