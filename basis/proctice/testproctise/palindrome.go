966deedef134f46e9e3f600bb60e8647f6d5106f
light <light@LightdeMacBook-Pro-2.local>
2020-02-27 15:10:25 +0800

    add time proctise

--- basis/proctice/testproctise/palindrome.go
+++ basis/proctice/testproctise/palindrome.go
@@ -1,7 +1,6 @@
 package testproctise
 
 import (
-	"fmt"
 	"strings"
 )
 
---
+++ basis/proctice/time01.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timeDemo() {
+	now := time.Now()
+	fmt.Printf("现在时间是:%v\n", now)
+
+	year := now.Year()
+	month := now.Month()
+	day := now.Day()
+	hour := now.Hour()
+	minute := now.Minute()
+	second := now.Second()
+	fmt.Printf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+}
+
+func timestampDemo() {
+	now := time.Now()
+	timestamp1 := now.Unix()
+	timestamp2 := now.UnixNano()
+	fmt.Printf("current timestamp1:%v\n", timestamp1)
+	fmt.Printf("current timestamp2:%v\n", timestamp2)
+}
+
+func timestampDemo2(timestamp int64) {
+	timeObj := time.Unix(timestamp, 0) //将时间戳转为时间格式
+	fmt.Println(timeObj)
+	year := timeObj.Year()     //年
+	month := timeObj.Month()   //月
+	day := timeObj.Day()       //日
+	hour := timeObj.Hour()     //小时
+	minute := timeObj.Minute() //分钟
+	second := timeObj.Second() //秒
+	fmt.Printf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+}
+
+func formatDemo() {
+	now := time.Now()
+	// 格式化的模板为Go的出生时间2006年1月2号15点04分 Mon Jan
+	// 24小时制
+	fmt.Println(now.Format("2006-01-02 15:04:05.000 Mon Jan"))
+	// 12小时制
+	fmt.Println(now.Format("2006-01-02 03:04:05.000 PM Mon Jan"))
+	fmt.Println(now.Format("2006/01/02 15:04"))
+	fmt.Println(now.Format("15:04 2006/01/02"))
+	fmt.Println(now.Format("2006/01/02"))
+}
+
+func tickDemo() {
+	ticker := time.Tick(time.Second) //定义一个1秒间隔的定时器
+	for i := range ticker {
+		fmt.Println(i)//每秒都会执行的任务
+	}
+}
